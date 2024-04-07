52120c4e33aaf03522c8c507760bafa3107576d9
lexusnikos <lexusnikos@mail.ru>
2024-04-07 13:20:00 +0300

    Виселица

--- src/Viselitsa.java
+++ src/Viselitsa.java
@@ -1,6 +1,4 @@
-import java.sql.Array;
 import java.util.ArrayList;
-import java.util.Objects;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -12,22 +10,13 @@ public class Viselitsa {
         System.out.println("Начать новую игру - отправьте \"Поехали\"\nЗакончить игру - отправьте \"Стоп\".");
         String answer = in.nextLine();
 
-        String[] words = new String[5];
-        words[0] = "ЛОГИКА";
-//            words[1] = "ЛОГАРИФМ";
-//            words[2] = "КУЛЬТУРА";
-//            words[3] = "СПОКОЙСТВИЕ";
-//            words[4] = "НАСЫЩЕННСОСТЬ";
+        String[] words = {"ЛОГИКА", "ЛОГАРИФМ", "КУЛЬТУРА", "СПОКОЙСТВИЕ", "НАСЫЩЕННСОСТЬ"};
 
-//            Random r = new Random();
-//
-//            int random_index_array_word = r.nextInt(words.length);
-//            String hidden_word = words[random_index_array_word];
-        String hidden_word = words[0];
+        Random r = new Random();
+        int random_index_array_word = r.nextInt(words.length);
+        String hidden_word = words[random_index_array_word];
 
-
-        if (answer.equals("Поехали") || answer.equals("поехали") || answer.equals("1")) {
-
+        if (answer.equalsIgnoreCase("Поехали") || answer.equals("поехали") || answer.equals("1")) {
 
             System.out.println("Начинается игра...");
             String[] letter_arr = new String[hidden_word.length()];
@@ -35,19 +24,17 @@ public class Viselitsa {
                 letter_arr[i] = "_";
                 System.out.print(letter_arr[i] + " ");
             }
-            int guessed_letters = 0;
-            int mistakes = 0;
-//                    String[] entered_letters = new String[] {};
 
-            ArrayList<String> entered_letters = new ArrayList<String>();
-            StringBuilder result = new StringBuilder();
+            int mistakes = 0;
+            ArrayList<String> entered_letters = new ArrayList<>();
+
             do {
                 System.out.println("\nВведите букву: ");
                 String suggest_letter = in.nextLine().toUpperCase();
 
                 boolean alreadyEntered = false;
-                for (int i = 0; i < entered_letters.size(); i++) {
-                    if (suggest_letter.equals(entered_letters.get(i))) {
+                for (String letter : entered_letters) {
+                    if (suggest_letter.equals(letter)) {
                         alreadyEntered = true;
                         break;
                     }
@@ -56,60 +43,69 @@ public class Viselitsa {
                     System.out.println("Данная буква уже была использована ранее, введите другую букву.");
                 } else {
                     entered_letters.add(suggest_letter);
+                    boolean guessed_correctly = false;
                     for (int i = 0; i < hidden_word.length(); i++) {
                         if (suggest_letter.equals(String.valueOf(hidden_word.charAt(i)))) {
-                            guessed_letters++;
                             letter_arr[i] = suggest_letter.toUpperCase();
-                            System.out.print(letter_arr[i] + " ");
-                            result.append(letter_arr[i]);
-                        } else if (!suggest_letter.equals(String.valueOf(hidden_word.charAt(i)))) {
-                            System.out.print(letter_arr[i] + " ");
+                            guessed_correctly = true;
                         }
                     }
-                    if (guessed_letters == 0) {
+                    if (!guessed_correctly) {
                         mistakes++;
                         Mistake(mistakes);
                         if (mistakes == 5) {
                             break;
                         }
                     }
+                    for (String letter : letter_arr) {
+                        System.out.print(letter + " ");
+                    }
                 }
-            }
-            while (guessed_letters != letter_arr.length);
+            } while (!isWordGuessed(letter_arr));
 
-            if (result.toString().equals(hidden_word)) {
+            if (isWordGuessed(letter_arr)) {
                 System.out.println("\nВы отгадали слово. Конец игры.");
             } else {
                 System.out.println("\nВы проиграли.");
             }
-
-
-        } else if (answer.equals("Стоп") || answer.equals("стоп")) {
-            System.out.print("Выход.");
+        } else if (answer.equalsIgnoreCase("Стоп")) {
+            System.out.println("Выход.");
         }
-
     }
 
     public static void Mistake(int number_of_errors) {
-        if (number_of_errors == 1) {
-            System.out.print("|     |\n|     |\n|     O\n|\n|\n|\n|\n");
-            System.out.println("___________");
-            System.out.println("Вы истратили 1/5 попыток.");
-        } else if (number_of_errors == 2) {
-            System.out.print("|     |\n|     |\n|     O\n|     |\n|     |\n|\n|\n|\n");
-            System.out.println("___________");
-            System.out.println("Вы истратили 2/5 попыток.");
-        } else if (number_of_errors == 3) {
-            System.out.print("|     |\n|     |\n|     O\n|    /|\n|     |\n|\n|\n|\n");
-            System.out.println("___________");
-            System.out.println("Вы истратили 3/5 попыток.");
-        } else if (number_of_errors == 4) {
-            System.out.print("|     |\n|     |\n|     O\n|    /|\\\n|     \n|\n|\n|\n");
-            System.out.println("___________");
-            System.out.println("Вы истратили 4/5 попыток.");
-        } else if (number_of_errors == 5) {
-            System.out.print("|     |\n|     |\n|     O\n|    /|\\\n|    / \\\n|\n|\n|\n");
-            System.out.println("___________");
+        switch (number_of_errors) {
+            case 1:
+                System.out.println("|     |\n|     |\n|     O\n|\n|\n|\n|\n___________");
+                System.out.println("Вы истратили 1/5 попыток.");
+                break;
+            case 2:
+                System.out.println("|     |\n|     |\n|     O\n|     |\n|     |\n|\n|\n___________");
+                System.out.println("Вы истратили 2/5 попыток.");
+                break;
+            case 3:
+                System.out.println("|     |\n|     |\n|     O\n|    /|\n|     |\n|\n|\n___________");
+                System.out.println("Вы истратили 3/5 попыток.");
+                break;
+            case 4:
+                System.out.println("|     |\n|     |\n|     O\n|    /|\\\n|     \n|\n|\n___________");
+                System.out.println("Вы истратили 4/5 попыток.");
+                break;
+            case 5:
+                System.out.println("|     |\n|     |\n|     O\n|    /|\\\n|    / \\\n|\n|\n___________");
+                System.out.println("Вы истратили 5/5 попыток. Конец игры.");
+                break;
+            default:
+                System.out.println("Неизвестное количество ошибок.");
         }
+    }
+
+    public static boolean isWordGuessed(String[] letters) {
+        for (String letter : letters) {
+            if (letter.equals("_")) {
+                return false;
+            }
+        }
+        return true;
     }
 }
